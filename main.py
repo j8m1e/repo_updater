88ee3506920ffd9399849bb231136b5dcf63aa65
Jamie Xu <jamiexu.ut@gmail.com>
2024-08-10 16:01:20 -0400

    "updated ./main.py"

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 import datetime
-import time
 import git_commit
+import argparse
 
-def list_file_dates(directory):
+def list_file_dates(directory,test):
     print()
     file_dates = {}
     exclude_prefixes = ('__', '.')  # exclusion prefixes
-    for root, dirs, files in os.walk(directory):
-        
+    os.chdir(directory)
+    if '.git' not in os.listdir(directory):
+        print("git isn't initialized, initializing")
+    for root, dirs, files in os.walk('.'):
         dirs[:] = [dirname for dirname in dirs if not dirname.startswith(exclude_prefixes)]
         files = [x for x in files if not x.startswith(exclude_prefixes)]
         for file_name in files:
@@ -22,19 +24,40 @@ def list_file_dates(directory):
 
             baseline = datetime.datetime(2020, 5, 17,23, 55, 59, 342380)
             lst = []
-            for i in [created,modified,accessed]:
+            for i in [modified,accessed]:
                 if i > baseline:
                     lst.append(i)
             
             print(f"  File: {file_path}")
-            # print(f"  Created: {created}")
-            # print(f"  Modified: {modified}")
-            # print(f"  Accessed: {accessed}")
-
-            print(min(lst))
-            git_commit.contribute(min(lst),file_path)
+            print(f"  Created: {created}")
+            print(f"  Modified: {modified}")
+            print(f"  Accessed: {accessed}")
+            print(f" Date: {min(lst)}")
+            if not test:
+                git_commit.contribute(min(lst),file_path)
     return
 
 # Example usage
-directory_path = '.'
-list_file_dates(directory_path)
+parser = argparse.ArgumentParser(description='push commits dated on file update date')
+parser.add_argument('directory', default = '.',
+                    help='directory that will be commited and pushed')
+parser.add_argument('-T','--test', action='store_true', default=False,
+                    help='test the list without commiting')
+
+args = parser.parse_args()
+test = args.test
+directory_path = args.directory
+
+
+list_file_dates(directory_path,test)
+
+if not test:
+    git_commit.run(['git', 'push'])
+    print('\nRepository generation ' +
+            '\x1b[6;30;42mcompleted successfully\x1b[0m!')
+
+
+# git init
+# gh repo create [<name>] --private -s .
+
+#git push origin main
